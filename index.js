8744bd9e14ef14630060c7793bd3fd95f41bb35f
Lidobix <pipoflutepouet@gmail.com>
2024-09-06 16:04:43 +0200

    refacto

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
 const INPUT_QTY = 5;
 let selectedIndex;
-let letters;
+const letters = [];
 let endGame;
 
 window.addEventListener('DOMContentLoaded', function () {
-  createGameBoard();
-  addKeyboardEvents();
+  startGame();
 });
 
-const createGameBoard = () => {
-  selectedIndex = 0;
-  letters = [];
-  endGame = false;
-  letters = determineMissingLetters();
-  const indexes = letters.map((letter) => {
-    return letter.index;
-  });
-
-  for (let i = 0; i < 26; i++) {
-    if (indexes.includes(i)) {
-      createInput(i);
-    } else {
-      createLetter(i);
-    }
-  }
-
-  letters[0].isSelected = true;
-  updateInputsStatus();
+startGame = () => {
+  createInitialDatas();
+  createGameBoard();
+  addKeyboardEvents();
 };
 
-const updateInputsStatus = () => {
-  for (let i = 0; i < letters.length; i++) {}
-  letters.forEach((letter) => {
-    const input = this.document.getElementById(`input_${letter.index}`);
+createGameBoard = () => {
+  endGame = false;
 
-    input.innerText = letter.currentValue;
-    letter.isValid = letter.targetValue === letter.currentValue;
+  const gameContainer = document.createElement('div');
+  gameContainer.id = 'gameBoard';
+  const body = document.querySelector('body');
+  body.appendChild(gameContainer);
 
-    if (letter.selected) {
-      if (letter) {
-      }
-    }
+  createLetters();
+  // updateLettersStatus();
+};
 
-    if (letter.isValid) {
-      input.classList.remove('selected');
-      input.classList.remove('unselected');
-      input.classList.remove('notValid');
-      input.classList.add('valid');
-    } else if (!letter.isValid && letter.currentValue !== '') {
-      input.classList.remove('selected');
-      input.classList.remove('unselected');
-      input.classList.remove('valid');
-      input.classList.add('notValid');
-    }
+updateLettersStatus = () => {
+  // for (let i = 0; i < letters.length; i++) {}
+  letters.forEach((letter, index) => {
+    // console.log(letter);
+    const divLetter = document.getElementById(`letter_${index}`);
+    divLetter.innerText = letter.currentValue;
+    divLetter.classList.add(letter.isInput ? 'letterInput' : 'letterFixed');
 
+    // if (letter.isValid) {
+    //   input.classList.remove('selected');
+    //   input.classList.remove('unselected');
+    //   input.classList.remove('notValid');
+    //   input.classList.add('valid');
+    // } else if (!letter.isValid && letter.currentValue !== '') {
+    //   input.classList.remove('selected');
+    //   input.classList.remove('unselected');
+    //   input.classList.remove('valid');
+    //   input.classList.add('notValid');
+    // }
     if (letter.isSelected) {
       if (!letter.isValid) {
-        input.classList.remove('notValid');
+        divLetter.classList.remove('notValid');
         // input.classList.add('notValidSelected');
       }
-      input.classList.remove('unselected');
-      input.classList.add('selected');
+      divLetter.classList.remove('unselected');
+      divLetter.classList.add('selected');
     } else {
-      input.classList.remove('selected');
+      divLetter.classList.remove('selected');
     }
   });
 
-  updateDataLetters();
+  // updateDatas();
 };
 
 const checkKeyboard = (code) => {
@@ -93,11 +83,11 @@ const checkKeyboard = (code) => {
   return `Key${letter}`;
 };
 
-const addKeyboardEvents = () => {
+addKeyboardEvents = () => {
   let keyDown = false;
 
-  this.document.addEventListener('keydown', (e) => {
-    console.log(e.key);
+  document.addEventListener('keydown', (e) => {
+    // console.log(e.key);
     if (!keyDown) {
       if (e.key === 'Tab') {
         return;
@@ -105,39 +95,49 @@ const addKeyboardEvents = () => {
 
       if (e.key === 'Backspace') {
         keyDown = true;
-        letters[selectedIndex].currentValue = '';
-        updateInputsStatus();
+        // letters[selectedIndex].currentValue = '';
+        // updateLettersStatus();
+        updateDatas(e.key);
       }
 
       if (checkKeyboard(e.code) === `Key${e.key.toUpperCase()}`) {
         keyDown = true;
-        letters[selectedIndex].currentValue = e.key;
-        updateInputsStatus();
+        // letters[selectedIndex].currentValue = e.key;
+        // updateLettersStatus();
+        updateDatas(e.key);
       }
 
-      if (e.key === 'ArrowRight' && selectedIndex < letters.length - 1) {
-        letters[selectedIndex].isSelected = false;
-        selectedIndex++;
-        letters[selectedIndex].isSelected = true;
-        updateInputsStatus();
+      if (
+        e.key === 'ArrowRight'
+        //  && selectedIndex < letters.length - 1
+      ) {
+        // letters[selectedIndex].isSelected = false;
+        // selectedIndex++;
+        // letters[selectedIndex].isSelected = true;
+        // updateLettersStatus();
+        updateDatas(e.key);
       }
 
-      if (e.key === 'ArrowLeft' && selectedIndex > 0) {
-        letters[selectedIndex].isSelected = false;
-        selectedIndex--;
-        letters[selectedIndex].isSelected = true;
-        updateInputsStatus();
+      if (
+        e.key === 'ArrowLeft'
+        //  && selectedIndex > 0
+      ) {
+        // letters[selectedIndex].isSelected = false;
+        // selectedIndex--;
+        // letters[selectedIndex].isSelected = true;
+        // updateLettersStatus();
+        updateDatas(e.key);
       }
 
       if (e.key === ' ' && endGame) {
         const modal = document.getElementById('winModal');
         modal.classList.add('notVisible');
         modal.classList.remove('show');
-        createGameBoard();
+        startGame();
       }
 
       keyDown = true;
-      this.setTimeout(() => {
+      setTimeout(() => {
         if (keyDown) {
           keyDown = false;
         }
@@ -150,47 +150,63 @@ const addKeyboardEvents = () => {
   });
 };
 
-const updateDataLetters = () => {
-  letters.forEach((letter, index) => {
-    if (letter.isValid && letters.length > 1) {
-      letters.splice(index, 1);
-      defineNextInput();
-      updateInputsStatus();
-    } else if (letter.isValid && letters.length === 1) {
-      winningModal();
-      endGame = true;
-    }
-  });
+const updateDatas = (letterValue) => {
+  console.log('letter+' + letterValue + '+Value', selectedIndex);
+  letters[selectedIndex].currentValue =
+    letterValue === 'Backspace' ? '' : letterValue;
+
+  // letters.forEach((letter, index) => {
+  //   if (letter.isValid && letters.length > 1) {
+  //     letters.splice(index, 1);
+  //     defineNextInput();
+  //     updateLettersStatus();
+  //   } else if (letter.isValid && letters.length === 1) {
+  //     openWinningModal();
+  //     endGame = true;
+  //   }
+  // });
+
+  updateLettersStatus();
 };
 
-const defineNextInput = () => {
-  if (selectedIndex >= letters.length) {
-    selectedIndex--;
+// const defineNextInput = () => {
+//   if (selectedIndex >= letters.length) {
+//     selectedIndex--;
+//   }
+//   letters[selectedIndex].isSelected = true;
+// };
+
+createInitialDatas = () => {
+  const missingLetters = determineMissingLetters();
+  selectedIndex = missingLetters[0];
+
+  for (let i = 0; i < alphabet.length; i++) {
+    const isInput = missingLetters.includes(i);
+
+    const letter = {
+      targetValue: alphabet[i],
+      currentValue: isInput ? '' : alphabet[i],
+      isInput,
+      isValid: !isInput,
+      isSelected: i === selectedIndex,
+    };
+    letters[i] = letter;
   }
-  letters[selectedIndex].isSelected = true;
 };
 
 const determineMissingLetters = () => {
   const selectedLetters = [];
 
   for (let i = 0; i < INPUT_QTY; i++) {
-    let selectedLetter = chooseLetter();
+    let selectedLetter = getLetterIndex();
 
     while (selectedLetters.includes(selectedLetter)) {
-      selectedLetter = chooseLetter();
+      selectedLetter = getLetterIndex();
     }
     selectedLetters.push(selectedLetter);
-
-    letters.push({
-      index: selectedLetter,
-      targetValue: alphabet[selectedLetter],
-      currentValue: '',
-      isValid: false,
-      isSelected: false,
-    });
   }
 
-  return letters.sort((a, b) => a.index - b.index);
+  return selectedLetters.sort((a, b) => a - b);
 };
 
 const getRandomInt = (min, max) => {
@@ -199,46 +215,42 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 };
 
-const createLetter = (letter) => {
-  const resContainer = document.getElementById('resContainer');
-  const newLetter = document.createElement('div');
-  newLetter.innerText = alphabet[letter];
-  newLetter.classList.add('letterContainer', 'letterFixed');
+const createLetters = () => {
+  // console.log('vreate letters');
+  const gameBoard = document.getElementById('gameBoard');
+  for (let i = 0; i < letters.length; i++) {
+    const newLetter = document.createElement('div');
+    newLetter.id = `letter_${i}`;
+    newLetter.innerText = letters[i].currentValue;
+    newLetter.classList.add('letterContainer');
+    gameBoard.appendChild(newLetter);
+  }
 
-  resContainer.appendChild(newLetter);
+  updateLettersStatus();
 };
 
-const createInput = (index) => {
-  const resContainer = document.getElementById('resContainer');
-  const inputLetter = document.createElement('div');
-  inputLetter.id = `input_${index}`;
-  inputLetter.classList.add('letterContainer', 'letterInput');
+// const openWinningModal = () => {
+//   const ninja = getNinja();
 
-  resContainer.appendChild(inputLetter);
-};
+//   const line1 = document.getElementById('line1');
+//   const line2 = document.getElementById('line2');
+//   line1.style.color = ninja.color1;
+//   line2.style.color = ninja.color2;
 
-const winningModal = () => {
-  const ninja = getNinja();
+//   const ninjaImage = `url(./assets/${ninja.name}.png)`;
+//   const modal = document.getElementById('winModal');
+//   modal.style.backgroundImage = ninjaImage;
+//   modal.classList.remove('notVisible');
+//   modal.classList.add('show');
+// };
 
-  const line1 = document.getElementById('line1');
-  const line2 = document.getElementById('line2');
-  line1.style.color = ninja.color1;
-  line2.style.color = ninja.color2;
-
-  const ninjaImage = `url(./assets/${ninja.name}.png)`;
-  const modal = document.getElementById('winModal');
-  modal.style.backgroundImage = ninjaImage;
-  modal.classList.remove('notVisible');
-  modal.classList.add('show');
-};
-
-const chooseLetter = () => {
+const getLetterIndex = () => {
   return getRandomInt(0, alphabet.length);
 };
 
-const getNinja = () => {
-  return ninjas[getRandomInt(0, ninjas.length)];
-};
+// const getNinja = () => {
+//   return ninjas[getRandomInt(0, ninjas.length)];
+// };
 
 const alphabet = [
   'a',
@@ -269,10 +281,10 @@ const alphabet = [
   'z',
 ];
 
-const ninjas = [
-  { name: 'all', color1: 'red', color2: 'red' },
-  { name: 'cole', color1: 'white', color2: 'white' },
-  { name: 'kai', color1: 'white', color2: 'white' },
-  { name: 'lloyd', color1: 'white', color2: 'white' },
-  { name: 'zane', color1: 'white', color2: 'white' },
-];
+// const ninjas = [
+//   { name: 'all', color1: 'red', color2: 'red' },
+//   { name: 'cole', color1: 'white', color2: 'white' },
+//   { name: 'kai', color1: 'white', color2: 'white' },
+//   { name: 'lloyd', color1: 'white', color2: 'white' },
+//   { name: 'zane', color1: 'white', color2: 'white' },
+// ];
